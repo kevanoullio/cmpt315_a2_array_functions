af437f04707da822b57c45e300138906d6667391
Kevin Ulliac <kevanoullio@outlook.com>
2024-01-22 19:32:11 -0700

    Added countNumberOfArrays() and test file.

--- countNumberOfArrays.js
+++ countNumberOfArrays.js
@@ -1,1 +1,16 @@
-const _ = require("cmpt315_a2_array_functions");
+/**
+ * Returns the count for the number of arrays inside a given array.
+ * @param {Array} array An array of numbers and/or arrays.
+ * @returns {number} The number of arrays inside the given array.
+ */
+function countNumberOfArrays(array) {
+    let count = 0;
+    for (let i = 0; i < array.length; i++) {
+        if (Array.isArray(array[i])) {
+            count++;
+        }
+    }
+    return count;
+}
+
+module.exports = countNumberOfArrays;
