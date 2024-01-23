1e067d97d6ae3aaa1a6792d5b6d373663cbf0af2
Kevin Ulliac <kevanoullio@outlook.com>
2024-01-22 19:49:57 -0700

    Added participants() and winnersObject() and biggestLoser() functions.

--- participants.js
+++ participants.js
@@ -1,1 +1,16 @@
-const _ = require("cmpt315_a2_array_functions");
+/**
+ * Returns an array of all the player names (no player should be listed more than once).
+ * @param {Array} array An array of objects representing players.
+ * @returns {Array} An array of player names.
+ */
+function participants(array) {
+    let names = [];
+    for (let i = 0; i < array.length; i++) {
+        if (!names.includes(array[i].name)) {
+            names.push(array[i].name);
+        }
+    }
+    return names;
+}
+
+module.exports = participants;
--- winnersObject.js
+++ winnersObject.js
@@ -1,1 +1,20 @@
-const _ = require("cmpt315_a2_array_functions");
+/**
+ * Returns an object which lists each participant, and a list of who they've beaten.
+ * @param {Array} array An array of objects representing players.
+ * @returns {Object} An object which lists each participant, and a list of who they've beaten.
+ */
+function winnersObject(array) {
+    let winners = {};
+    for (let i = 0; i < array.length; i++) {
+        let winner = array[i].winner;
+        let loser = array[i].loser;
+        if (winners[winner] === undefined) {
+            winners[winner] = [loser];
+        } else {
+            winners[winner].push(loser);
+        }
+    }
+    return winners;
+}
+
+module.exports = winnersObject;
