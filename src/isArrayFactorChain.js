04af0e76d0c3e867c25ba83c0ef67beee9723261
Kevin Ulliac <kevanoullio@outlook.com>
2024-01-23 01:23:07 -0700

    Fixed but in isArrayFactorChain(), added more test code in test.js.

--- src/isArrayFactorChain.js
+++ src/isArrayFactorChain.js
@@ -4,11 +4,13 @@
  * @returns {boolean} True if the array is a factor chain, false otherwise.
  */
 function isArrayFactorChain(array) {
+    // An array of less than 2 elements cannot be a factor chain.
     if (array.length < 2) {
         return false;
     }
+    // Check if each element is a factor of the next element.
     for (let i = 0; i < array.length - 1; i++) {
-        if (array[i] % array[i + 1] !== 0) {
+        if (array[i + 1] % array[i] !== 0) {
             return false;
         }
     }
